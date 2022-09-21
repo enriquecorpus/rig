e8dcd3f1604122274bd17060f792f3f5d5260fab
Enrique Corpus <enriquecorpus@skydev.solutions>
2022-09-21 10:58:04 +0800

    download image

--- main.py
+++ main.py
@@ -2,69 +2,85 @@ from fastapi import FastAPI
 import csv
 import random
 import datetime
+import uuid
+import base64
+import os
+from pathlib import Path
+from bing_image_downloader import bing as b
+
 
 reader = csv.reader(open('names.csv', 'r'))
 dataset = {}
-for i,row in enumerate(reader):
-   name, gender,_,_ = row
-   dataset[i] = {
-       "name": name,
-       "gender": gender,
-   }
+for i, row in enumerate(reader):
+    name, gender, _, _ = row
+    dataset[i] = {
+        "name": name,
+        "gender": gender,
+    }
 
 
-class RandomPeople():
+class RandomPeople:
+    id = ""
+    name = ""
+    gender = ""
+    dob = ""
+    photo = ""
+    age = 0
+
     def __init__(self):
         self.generate_information()
 
-    def toJSON(self):
+    def to_json(self):
         return {
-            "name" : self.name,
+            "name": self.name,
             "gender": self.gender,
-            "dob" : self.dob,
-            "photo": self.random_photo()
+            "dob": self.dob,
+            "age": self.age,
+            "photo": self.photo
         }
 
     def generate_information(self):
         data = dataset[random.randrange(0, len(dataset))]
+        dob = self.random_birthday()
+        self.id = str(uuid.uuid4())
         self.name = data["name"]
         self.gender = data["gender"]
-        self.dob = self.random_birthday()
+        self.dob = dob.strftime("%m/%d/%Y")
+        self.age = datetime.datetime.now().year - dob.year
         self.photo = self.random_photo()
 
     def random_birthday(self):
-        end = datetime.datetime.now()
+        end = datetime.datetime.now() - datetime.timedelta(days=365)
         start = end - datetime.timedelta(days=(365 * 100)) 
-        return (start + datetime.timedelta(
+        return start + datetime.timedelta(
             seconds=random.randint(0, int((end - start).total_seconds())),
-        )).strftime("%m/%d/%Y")
+        )
 
     @property
     def humanize_gender(self):
         return "woman" if self.gender == "f" else "man"
+
     def random_photo(self):
-        # of images to download. ("tall, square, wide, panoramic")
-        import pdb
-        pdb.set_trace()
-        arguments = {"keywords": "face of a {}".format(self.gender),
-                    "format": "jpg",
-                    "limit":4,
-                    "print_urls":True,
-                    "size": "medium",
-                    "aspect_ratio":"panoramic"}
-        x = response.download(arguments)
+        output_dir = Path(str(self.id)).absolute()
+        if not Path.is_dir(output_dir):
+            Path.mkdir(output_dir, parents=True)
+        bing = b.Bing(query="face of a {} age of {}".format(self.humanize_gender, self.age),
+                      limit=1, output_dir=output_dir, adult="on", timeout=60,filter="imagesize-small")
+        bing.run()
+        file = os.listdir(output_dir)[0]
+        with open(Path(output_dir).joinpath(file).absolute(), "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        return encoded_string.decode("utf-8")
 
 
-    
+
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     random_people = RandomPeople()
-
-    return {"data": random_people.toJSON()}
+    return {"data": random_people.to_json()}
 
 @app.get("/hi")
 async def root():
